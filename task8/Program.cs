297bb7e631ae53a7374ef17d1e99ddd14aa98888
Podsh <your.spring@gmail.com>
2022-09-26 00:03:12 +0500

    Добавление task8

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+Console.Write($"Все четные числа от 1 до {numberN}: ");
+
+while (count != numberN+1)
+{
+if (count % 2 == 0) 
+{
+    Console.Write($"{count} ");
+}
+count++;
+}
