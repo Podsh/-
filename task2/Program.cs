e1f3e020d1dbafceda97ba02468df784e08c2bdb
Podsh <your.spring@gmail.com>
2022-09-25 23:07:06 +0500

    Добавление task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB) 
+{
+    Console.WriteLine($"Максимальное значение - {numberA}, минимальное значение - {numberB}");
+}
+else if (numberA < numberB)
+{
+    Console.WriteLine($"Максимальное значение - {numberB}, минимальное значение - {numberA}");
+}
+else 
+{
+    Console.WriteLine($"Большего значения нет, т.к. значения равны между собой");
+}
