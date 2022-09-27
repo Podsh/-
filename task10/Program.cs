10bc4238d9936e5e67b6ac111efdefaaadd9cf5c
Podsh <your.spring@gmail.com>
2022-09-27 17:44:25 +0500

    Добавление task10

---
+++ task10/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int getNewDigitFromNumber(int number)
+{
+    int secondDigit = number % 100;
+    secondDigit = secondDigit / 10;
+    return secondDigit;
+}
+
+int secondDigit = getNewDigitFromNumber(number);
+Console.WriteLine($"Для числа {number} вторая цифра {secondDigit}");
---
+++ task11/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, 
+которая выводит случайное трёхзначное число 
+и удаляет вторую цифру этого числа. 
+456 -> 46 782 -> 72 918 -> 98*/
+
+int number = new Random().Next(100, 1000);
+
+int getDeleteTwoDigitFromNumber(int number)
+{
+    int maxDigit = 0;
+    while (number > 0)
+    {
+        int currentDigit = number % 10;
+        if(maxDigit < currentDigit)
+        {
+            maxDigit = currentDigit;
+        }
+        number = number / 10;
+        Console.WriteLine(number);
+    }
+    return maxDigit;
+}
+{
+    for (int i = 0; i < 3; i++);
+    {
+        
+    }
+}
+
+Console.WriteLine(number);
+Console.WriteLine(getDeleteTwoDigitFromNumber(number));
