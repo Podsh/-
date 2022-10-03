c144bdddb133ab6443a7380b939ef5e8a6e75e74
Podsh <your.spring@gmail.com>
2022-10-03 13:09:52 +0500

    Добавление task20

---
+++ task20/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 2D пространстве */
+
+
+
+double LenghtFromCoordinates()
+{
+    Console.WriteLine("Введите значение точки 1 координаты X");
+    int tochkaX1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение точки 1 координаты Y");
+    int tochkaY1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите значение точки 2 координаты X");
+    int tochkaX2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение точки 2 координаты Y");
+    int tochkaY2 = Convert.ToInt32(Console.ReadLine());
+   
+    double result = Math.Sqrt(Math.Pow(tochkaX1 - tochkaX2, 2) + Math.Pow(tochkaY1 - tochkaY2, 2));
+    return result;
+} 
+
+double result = LenghtFromCoordinates();
+Console.WriteLine(result);
+
+
+
