37ceca4b049d11f5c1e5b815ffe2d144b336dca1
Podsh <your.spring@gmail.com>
2022-10-04 14:18:07 +0500

    Добавление task25

---
+++ task25/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+int getNumbersFromUser(string label)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(label);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if(result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Введите целое число, вы ввели {userLine}");
+        }
+    }
+    return result;
+}
+
+int getDegree (int userNumberA, int userNumberB)
+{
+    int result = userNumberA;
+    for (int i = 1; i < userNumberB; i++)
+    {
+        result = result * userNumberA;
+    }
+    return result;
+}
+
+int userNumberA = 0;
+int userNumberB = 0;
+Console.WriteLine("Введите число A");
+userNumberA = getNumbersFromUser("A: ");
+Console.WriteLine("Введите число B");
+userNumberB = getNumbersFromUser("B: ");
+int resultOfDegree = getDegree(userNumberA, userNumberB);
+Console.WriteLine($"Результат возведения числа {userNumberA} в степень {userNumberB} равен {resultOfDegree}");
+
+
+
+
+
+
