c9e652776fb7c62c09601c9421c2b3a0855573ee
Podsh <your.spring@gmail.com>
2022-10-04 13:05:18 +0500

    Изменение в task26

--- task26/Program.cs
+++ task26/Program.cs
@@ -3,7 +3,21 @@
 78 -> 2
 89126 -> 5 */
 
+//Console.WriteLine("Введите число");
+//string userNumber = Console.ReadLine();
+// char[] array = userNumber.ToCharArray();
+//int result = array.Length;
+// string length = Convert.ToString (userNumber);
+// int Length = Text.Length;
+//Console.WriteLine($"Количество знаков в числе - {result}");
+
+
 Console.WriteLine("Введите число");
 int userNumber = Convert.ToInt32(Console.ReadLine());
-int result = (int)Math.Log10(userNumber)+ 1;
+userNumber = Math.Abs(userNumber);
+int result = 1;
+if (userNumber != 0)
+{
+    result = (int)Math.Log10(userNumber) + 1;
+}
 Console.WriteLine($"Количество знаков в числе - {result}");
--- task28/Program.cs
+++ task28/Program.cs
@@ -2,17 +2,16 @@
 4 -> 24
 5 -> 120 */
 
-/* Console.WriteLine("Введите число");
-int userNumber = Convert.ToInt32(Console.ReadLine());
-int result = (int)Math.Log10(userNumber) + 1;
-Console.WriteLine($"Количество знаков в числе - {result}"); */
+/* 
+int result = Math.Factorial(userNumber);
+Console.WriteLine($"Произведение чисел равно {result}"); */
 
 Console.WriteLine("Введите число");
 int userNumber = Convert.ToInt32(Console.ReadLine());
 int result = 1;
 for (int i = 1; i <= userNumber; i++)
 {
-    result = result * result;
+    result = result * i;
 }
 Console.WriteLine($"Произведение чисел от 1 до {userNumber} равно {result}");
 
