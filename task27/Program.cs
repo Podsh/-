bb9b62560bd9d6713a7b0dc2b7c9360884154eb6
Podsh <your.spring@gmail.com>
2022-10-04 19:18:37 +0500

    Добавление task30

--- task27/Program.cs
+++ task27/Program.cs
@@ -4,21 +4,24 @@
 82 -> 10
 9012 -> 12 */
 
-int getSumOfDigits(int userNumber)
+
+int getSumOfDigits(string userNumberString)
 {
     int result = 0;
-    int buff = 0;
-    int userNumberLength = Convert.ToString(userNumber).Length;
+    int tmp = 0;
+    int userNumber=0;
+    userNumber = Convert.ToInt32(userNumberString);
+    int userNumberLength = (userNumberString).Length;
     for (int i = 0; i < userNumberLength; i++)
     {
-        buff = userNumber - userNumber % 10;
-        result = result + (userNumber - buff);
+        tmp = userNumber % 10;
+        result = result + tmp;
         userNumber = userNumber / 10;
     }
     return result;
 }
 
 Console.WriteLine("Введите число");
-int userNumber = Convert.ToInt32(Console.ReadLine());
+string userNumber = Console.ReadLine();
 int sumOfDigits = getSumOfDigits(userNumber);
 Console.WriteLine($"Сумма цифр числа {userNumber} равна {sumOfDigits}");
---
+++ task29/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 29: Напишите программу, в которой пользователь задает длину массива, 
+элементы которого задаются в диапазоне [1,99] и выводит на экран.
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33] */
+
+Console.WriteLine("Введите цифру, задающую размер массива");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int[] array = getArray(userNumber);
+Console.WriteLine($"Вывод рандомных {userNumber} чисел в диапазоне от 1 до 99:");
+printArray(array);
+
+int[] getArray (int userNumber)
+{
+    int[] array = new int[userNumber];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,100);
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+
+}
+
+
