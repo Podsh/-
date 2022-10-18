7ec4320f3e08af41bd2c54caa066ef12d2cf73e8
Podsh <your.spring@gmail.com>
2022-10-18 15:44:06 +0500

    Добавление task67

---
+++ task67/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 67: Напишите программу, которая будет принимать на вход число 
+и возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+GetSum(userNumber);
+Console.WriteLine(GetSum(userNumber));
+
+int GetSum(int N)
+{
+    if (N == 0)
+    {
+        return 0;
+    }
+    int digit = N % 10;
+    return digit + GetSum(N / 10);
+}
+
